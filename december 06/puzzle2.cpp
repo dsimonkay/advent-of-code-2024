df4814aba55fec80144b3904188a93febcd6287a
Daniel Simonkay <dsimonkay@gmail.com>
2024-12-07 01:17:45 +0100

    Add December 06, puzzle2 -- slow and ugly, but working solution

--- december 06/puzzle2.cpp
+++ december 06/puzzle2.cpp
@@ -20,33 +20,32 @@ bool IsWithinMap(const int row, const int col, const int max_row, const int max_
     return (0 <= row) && (row <= max_row) && (0 <= col) && (col <= max_col);
 }
 
-int GetNumberOfPossibleObstructions(const std::vector<std::vector<char>>& map,
-                                    const int starting_row,
-                                    const int starting_col,
-                                    const int starting_direction_idx = 0)
+bool ContainsLoop(const std::vector<std::vector<char>>& map,
+                  const int obstruction_row,
+                  const int obstruction_col,
+                  const int starting_row,
+                  const int starting_col,
+                  const int starting_direction,
+                  const int max_row,
+                  const int max_col)
 {
-    // Preconditions
-    assert(!map.empty());
-    assert(!map[0].empty());
-
-    const int max_row{static_cast<int>(map.size() - 1U)};
-    const int max_col{static_cast<int>(map[0].size() - 1U)};
-    assert(starting_row <= max_row);
-    assert(starting_col <= max_col);
-
+    // The tuple encodes the <row, col, direction> of a step of the guard's path 
+    std::set<std::tuple<int, int, int>> path{};
     int row{starting_row};
     int col{starting_col};
-    int direction{starting_direction_idx};
-
-    int nr_of_possible_obstructions = 0;
-
-    // The tuple encodes the <row, column, direction> of the guard at a given step
-    std::set<std::tuple<int, int, int>> path{};
+    int direction{starting_direction};
+    bool loop_found{false};
 
     while (true)
     {
+        if (path.contains({row, col, direction}))
+        {
+            loop_found = true;
+            break;
+        }
+
         const auto result = path.insert({row, col, direction});
-        assert(result.second); // Make sure the insertion was successful
+        assert(result.second);   // Make sure the insertion was successful
 
         // Lookahead: one step in the given direction
         const int new_row = row + moves[direction].first;
@@ -58,54 +57,54 @@ int GetNumberOfPossibleObstructions(const std::vector<std::vector<char>>& map,
             break;
         }
 
-        if (map[new_row][new_col] == '#')
+        if ((map[new_row][new_col] == '#') || ((new_row == obstruction_row) && (new_col == obstruction_col)))
         {
             direction = (direction + 1) % 4;   // Turn right
-            continue;
         }
-
-        // What would happen if there was an obstruction at (new_row, new_col) and we turned to the right
-        // at this very position (because of the new obstruction)? Would we end up hitting a known path?
-        int probe_direction{(direction + 1) % 4};
-        int probe_row{row};
-        int probe_col{col};
-        std::set<std::tuple<int, int, int>> probe_path{};
-        while (true)
+        else
         {
-            if (path.contains({probe_row, probe_col, probe_direction}) ||
-                probe_path.contains({probe_row, probe_col, probe_direction}))
+            row = new_row;
+            col = new_col;
+        }
+    }
+
+    return loop_found;
+}
+
+int GetNumberOfPossibleObstructions(const std::vector<std::vector<char>>& map,
+                                    const int starting_row,
+                                    const int starting_col,
+                                    const int starting_direction = 0)
+{
+    // Preconditions
+    assert(!map.empty());
+    assert(!map[0].empty());
+
+    const int max_row{static_cast<int>(map.size() - 1U)};
+    const int max_col{static_cast<int>(map[0].size() - 1U)};
+    assert(starting_row <= max_row);
+    assert(starting_col <= max_col);
+
+    int nr_of_possible_obstructions = 0;
+
+    // Put an extra obstruction at each available position in the map and check whether it contains then a loop.
+    // TODO: get the original path and only put obstacles along it.
+    for (int i{0}; i <= max_row; ++i)
+    {
+        std::cout << "Checking loops in row " << i << "...\n";
+        for (int j{0}; j <= max_col; ++j)
+        {
+            if (((i == starting_row) && (j == starting_col)) || map[i][j] == '#')
             {
-                // We have just discovered a loop!
-                std::cout << "Possible obstruction position: (" << new_row << ", " << new_col << ")\n";
+                // Don't put an obstacle on the starting position or over existing obstructions
+                continue;
+            }
+
+            if (ContainsLoop(map, i, j, starting_row, starting_col, starting_direction, max_row, max_col))
+            {
                 nr_of_possible_obstructions++;
-                break;
-            }
-
-            const auto probe_result = probe_path.insert({probe_row, probe_col, probe_direction});
-            assert(probe_result.second);
-
-            // Advance (hypothetically)
-            probe_row += moves[probe_direction].first;
-            probe_col += moves[probe_direction].second;
-
-            if (!IsWithinMap(probe_row, probe_col, max_row, max_col))
-            {
-                // Leaving the map; that was it
-                break;
-            }
-
-            if (map[probe_row][probe_col] == '#')
-            {
-                // Backtrack...
-                probe_row -= moves[probe_direction].first;
-                probe_col -= moves[probe_direction].second;
-                // ...and turn
-                probe_direction = (probe_direction + 1) % 4;
             }
         }
-
-        row = new_row;
-        col = new_col;
     }
 
     return nr_of_possible_obstructions;
@@ -131,14 +130,13 @@ int main()
     //     V: 2
     //     <: 3
     //    Note that in this order the direction turns 90 degrees as the index increases (modulo 4).
-    const char starting_direction{'^'};
-    const int starting_direction_idx{0};
+    const int starting_direction{0};
 
     int row_counter{0};
     while (std::getline(input_file, line))
     {
         map.push_back(std::vector<char>{line.begin(), line.end()});
-        const auto guard_pos = line.find(starting_direction);
+        const auto guard_pos = line.find('^');
         if (guard_pos != std::string::npos)
         {
             starting_row = row_counter;
@@ -149,7 +147,9 @@ int main()
     }
 
     input_file.close();
-    const int nr_of_possible_obstructions = GetNumberOfPossibleObstructions(map, starting_row, starting_col, starting_direction_idx);
+
+    assert((starting_row >= 0) && (starting_col >= 0));
+    const int nr_of_possible_obstructions = GetNumberOfPossibleObstructions(map, starting_row, starting_col, starting_direction);
     std::cout << "Number of possible obstructions: " << nr_of_possible_obstructions << std::endl;
 
     return 0;
